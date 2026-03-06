be5633c65720c3b9cd3b31e8c204adc7ac5cc642
atharva <atharvagupta1434@gmail.com>
2026-03-07 03:34:06 +0530

    auth & nav

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,49 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/header";
+import { shadesOfPurple } from "@clerk/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Work_Mtrix",
+  title: "Work_Matrix",
   description: "Project Management App",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ClerkProvider
+          appearance={{
+            baseTheme: shadesOfPurple,
+            variables: {
+              colorPrimary: "#3b82f6",
+              colorBackground: "#1a202c",
+              colorInputBackground: "#2D3748",
+              colorInputText: "#F3F4F6",
+            },
+            elements: {
+              card: "!bg-gray-800",
+              formButtonPrimary: "!bg-blue-600 hover:!bg-blue-700 !text-white",
+              headerTitle: "!text-blue-500",
+              headerSubtitle: "!text-gray-400",
+            },
+          }}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">{children}</main>
+
+            <footer className="py-6 text-center text-neutral-400 text-[0.95rem] border-t border-neutral-800 mt-8 bg-black bg-opacity-5">
+              <div className="container mx-auto text-center">
+                © Work_Matrix — All rights reserved
+              </div>
+            </footer>
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+     
       <h1>Hello World</h1>
     </div>
   );
